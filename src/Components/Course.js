21414cbc5a97df84056bb5d04a4e89bb7ff0f5fe
loukkevin <loukkevin@gmail.com>
2018-02-24 23:34:12 -0600

    update course information with api call when clicked

--- src/Components/Course.js
+++ src/Components/Course.js
@@ -8,6 +8,7 @@ class Course extends Component{
         prerequisites: [],
         credits: this.props.credits,
         description: this.props.name,
+        semestersOffered: [],
         isToggleOn: false
       }
 
@@ -18,6 +19,13 @@ class Course extends Component{
       this.setState(prevState => ({
         isToggleOn: !prevState.isToggleOn
       }));
+          let api = 'https://morning-temple-78184.herokuapp.com/getCourseInformation?name='
+              fetch(api + this.state.name)
+                  .then( (response) => {
+                      return response.json() })
+                          .then( (json) => {
+                              this.setState({prerequisites: json.prerequisites, credits: json.credits,  description: json.description, semestersOffered: json.semestersOffered});
+                          });
       if (this.state.isToggleOn){
         this.props.selectCourseHandler(this.state.credits,false);
       }else{
